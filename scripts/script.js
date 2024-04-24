744ab7aa6b67657337f8a1aedf1bec2078acafed
ToasterExpress <toasterexpress2789@gmail.com>
2024-04-24 16:17:20 -0600

    creating a navbar that lapses over the header

--- scripts/script.js
+++ scripts/script.js
@@ -92,3 +92,13 @@ function formatted(date = new Date()) {
 function short(num) {
   return num.toString().padStart(2, "0");
 }
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
